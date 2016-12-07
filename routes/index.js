fa88bcc49b3afb6a53ae923e13b5cdb3dfa912e8
maciul <pawel.maciulewski@gmail.com>
2016-12-07 00:10:40 +0000

    change localhost

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var dotenv = require('dotenv').config();
-const URLparse      = require('url').parse;
 
 /* GET home page. */
 router.get('/', function( req, res) {
